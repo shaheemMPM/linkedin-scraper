598042080f4b4ccbc50f0d6d0c291fcde8cc465e
shaheemMPM <shaheem.mpm@gmail.com>
2021-07-17 23:09:35 +0530

    Adds Initial Table

--- popup/popup.js
+++ popup/popup.js
@@ -7,10 +7,38 @@ btnAddSelect.onclick = () => {
       tabs[0].id,
       { action: "addButtons" },
       function (data) {
-        console.log("popup", data);
+        console.log("select : ", data);
       }
     );
   });
+};
+
+// select btnGetUsers button and add onclick listener
+const btnGetUsers = document.getElementById("btnGetUsers");
+
+btnGetUsers.onclick = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: "getUsers" },
+      function (data) {
+        populateTable(data.candidates);
+      }
+    );
+  });
+};
+
+const populateTable = (users) => {
+  const usersTable = document.getElementById("tbUsers");
+  usersTable.innerHTML = "<tr><td>Name</td><td>Action</td></tr>";
+
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    console.log(user);
+    const tempTR = document.createElement("tr");
+    tempTR.innerHTML = `<td>${user.userName}</td><td><a href="${user.userHref}" target="_blank">OPEN</a></td>`;
+    usersTable.appendChild(tempTR);
+  }
 };
 
 // chrome.storage.local.get("count", function (data) {

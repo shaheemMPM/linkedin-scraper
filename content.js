ea2c84a43274d25c55535d9cb4fbe806b7f404ca
shaheemMPM <shaheem.mpm@gmail.com>
2021-07-17 19:28:46 +0530

    Adds Select user button in search results

--- content.js
+++ content.js
@@ -15,11 +15,22 @@ const addSelectUserButtons = () => {
   let usersUL = document.getElementsByClassName(
     "search-results__result-list"
   )[0];
+
   let usersLiItems = usersUL.children;
+
   for (let i = 0; i < usersLiItems.length; i++) {
     let userLI = usersLiItems[i];
     let userLiInnerHTML = userLI.innerHTML;
     let selectUserBtn = '<button class="btn-select-user">Select user</button>';
     userLI.innerHTML = selectUserBtn + userLiInnerHTML;
+  }
+
+  // find all select user buttons and add onclick
+  const selectUserButtons = document.getElementsByClassName("btn-select-user");
+  for (let i = 0; i < selectUserButtons.length; i++) {
+    const btnSelectUser = selectUserButtons[i];
+    btnSelectUser.onclick = (event) => {
+      console.log("test", event.target);
+    };
   }
 };
